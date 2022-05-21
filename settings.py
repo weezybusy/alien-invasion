4290a94e42b467ea89d1014b91fff633f33a58e6
Vitaliy Pisnya <vitaliy.pisnya@gmail.com>
2022-05-21 15:20:36 +0300

    Move event checking loop to a separate helper function _check_events()

--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings."""
         # Screen settings
-        self.screen_width = 900
-        self.screen_height = 600
+        self.screen_width = 1366
+        self.screen_height = 704
         self.bg_color = (33, 33, 33)
