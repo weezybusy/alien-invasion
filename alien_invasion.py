5d90dde091fd0e89156abaf70f1c0b48b5ea85d2
Vitaliy Pisnya <vitaliy.pisnya@gmail.com>
2022-05-21 15:30:48 +0300

    Move code responsible for updating images on the screen to a separate helper function _update_screen()

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,19 +23,21 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self._update_screen()
 
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
 
     def _check_events():
-        # Watch for keyboard and mouse events.
+        """Respond to keypresses and mouse events."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+    def _update_screen():
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
