4a9c8a7f7261ff2af4c082450dfee40172883069
Vitaliy Pisnya <vitaliy.pisnya@gmail.com>
2022-05-24 14:41:23 +0300

    refactor: move update and remove bullets code to a separate helper method

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,11 +30,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-            # Get rid of bullets that have desappeared.
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
+            self._update_bullets()
             self._update_screen()
 
     def _check_events(self):
@@ -70,6 +66,14 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Update position of the bullets and get rid of the old ones."""
+        self.bullets.update()
+        # Get rid of bullets that have desappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
