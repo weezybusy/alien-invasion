a24e97532608b7a96d7596945c2491dcfe07a19a
Vitaliy Pisnya <vitaliy.pisnya@gmail.com>
2022-05-24 14:21:44 +0300

    perf: remove bullets when they are out of the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,6 +31,10 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self.bullets.update()
+            # Get rid of bullets that have desappeared.
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
             self._update_screen()
 
     def _check_events(self):
