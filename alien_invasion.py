8615398fa2e742123c9206a01790177315875c76
Vitaliy Pisnya <vitaliy.pisnya@gmail.com>
2022-05-29 17:02:54 +0300

    feat: stop the game after using all the ships

--- alien_invasion.py
+++ alien_invasion.py
@@ -39,9 +39,10 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
             self._update_screen()
 
     def _check_events(self):
@@ -104,11 +105,9 @@ class AlienInvasion:
         """
         self._check_fleet_edges()
         self.aliens.update()
-
         # Look for alien-ship collisions.
         if pygame.sprite.spritecollideany(self.ship, self.aliens):
             self._ship_hit()
-
         # Look for aliens hitting bottom of the screen.
         self._check_aliens_bottom()
 
@@ -134,7 +133,7 @@ class AlienInvasion:
             screen_rect = self.screen.get_rect()
             if alien.rect.bottom >= screen_rect.bottom:
                 # Treat this the same as the ship got hit.
-                self.ship_hit()
+                self._ship_hit()
                 break
 
     def _create_fleet(self):
