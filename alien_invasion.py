baeda0c9f475f85c7e30c732f4175c7ab5861087
Vitaliy Pisnya <vitaliy.pisnya@gmail.com>
2022-05-29 16:07:00 +0300

    feat: Look for alien-ship collisions

--- alien_invasion.py
+++ alien_invasion.py
@@ -103,6 +103,10 @@ class AlienInvasion:
         self._check_fleet_edges()
         self.aliens.update()
 
+        # Look for alien-ship collisions.
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            print("Ship hit!!!")
+
     def _create_fleet(self):
         """Create the fleet of aliens."""
         alien_width = self.settings.alien_width
