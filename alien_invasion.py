a4cc851c2ef37926cedb0dacd5a30322567fd916
Vitaliy Pisnya <vitaliy.pisnya@gmail.com>
2022-06-14 23:58:01 +0300

    feat: reset speed settings when play button pushed

--- alien_invasion.py
+++ alien_invasion.py
@@ -63,6 +63,8 @@ class AlienInvasion:
         """Start a new game when the player clicks Play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            # Reset the game settings.
+            self.settings.initialize_dynamic_settings()
             self._start_game()
 
     def _start_game(self):
