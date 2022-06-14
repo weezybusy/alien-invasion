2e4dcbed06977563c742936d2f6b6795b669b396
Vitaliy Pisnya <vitaliy.pisnya@gmail.com>
2022-06-14 23:52:51 +0300

    feat: increase speed of the ship, bullets and aliens every new level. refactor: separate dynamic settings.

--- alien_invasion.py
+++ alien_invasion.py
@@ -124,6 +124,7 @@ class AlienInvasion:
             # Remove existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _update_aliens(self):
         """
--- settings.py
+++ settings.py
@@ -13,13 +13,11 @@ class Settings:
         self.bg_color = (33, 33, 33)
 
         # Ship settings.
-        self.ship_speed = 5.0
         self.ship_width = 50
         self.ship_height = 50
         self.ship_limit = 1
 
         # Bullet settings.
-        self.bullet_speed = 5.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (255, 204, 0)
@@ -28,12 +26,25 @@ class Settings:
         # Alien settings.
         self.alien_width = 50
         self.alien_height = 50
-        self.alien_speed = 5.0
         self.fleet_drop_speed = 10
-        # Fleet direction of 1 represents right; -1 represents left.
-        self.fleet_direction = 1
+        self.speedup_scale = 1.1
 
         # Star settings.
         self.star_color = (255, 255, 255)
-        # The higher is value the lower is star density.
         self.star_distance = 20
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 1
+        self.fleet_direction = 1
+
+    def increase_speed(self):
+        """Increase speed settings."""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+
