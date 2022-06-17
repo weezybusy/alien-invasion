a3aac2aef0c84fa8d9af5b3cf92cc1493fbd4466
Vitaliy Pisnya <vitaliy.pisnya@gmail.com>
2022-06-17 17:13:10 +0300

    feat: detect and take account for bullet collision with more than one alien at a time

--- alien_invasion.py
+++ alien_invasion.py
@@ -152,7 +152,8 @@ class AlienInvasion:
         collisions = pygame.sprite.groupcollide(
                 self.bullets, self.aliens, True, True)
         if collisions:
-            self.stats.score += self.settings.alien_points
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
         if not self.aliens:
             # Remove existing bullets and create new fleet.
